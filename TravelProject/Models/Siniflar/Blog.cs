9db89bf9c87adcf3cb060cb751ca6f3d9f095b71
busranurbaydur <busranurbaydur@outlook.com>
2021-06-06 18:58:25 +0300

    Add project files.

---
+++ TravelProject/Models/Siniflar/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelProject.Models.Siniflar
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Baslik { get; set; }
+        public DateTime Tarih { get; set; }
+        public string Aciklama { get; set; }
+        public string FotoUrl { get; set; }
+    }
+}
