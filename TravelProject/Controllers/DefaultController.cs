c48e356ed2b437624d304ed4cf15b9a28f126209
busranurbaydur <busranurbaydur@outlook.com>
2021-06-09 00:59:28 +0300

    Homepage Edited

--- TravelProject/Controllers/DefaultController.cs
+++ TravelProject/Controllers/DefaultController.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelProject.Models.Siniflar;
 
 namespace Travel.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
         }
 
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.Id).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.Where(x => x.Id == 4).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial3()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.Id).Take(10).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial4()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.Id).Take(3).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial5()
+        {
+            var degerler = c.Blogs.Take(3).ToList();
+            return PartialView(degerler);
+        }
+
+        
     }
 }
