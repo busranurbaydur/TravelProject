7a473d0a6c05be871f70bd5dc9723c0ae4aaf422
busranurbaydur <busranurbaydur@outlook.com>
2021-06-18 02:00:23 +0300

    Admin Page Added

---
+++ TravelProject/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelProject.Models.Siniflar;
+
+namespace TravelProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var result = c.Blogs.ToList();
+            return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniBlog(Blog blog)
+        {
+            c.Blogs.Add(blog);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult BlogSil(int id)
+        {
+
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult BlogGetir(int id)
+        {
+            var bl = c.Blogs.Find(id);
+            return View("BlogGetir", bl);
+        }
+
+    }
+}
