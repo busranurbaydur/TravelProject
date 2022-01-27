46b9abdcb816c301bb04e743aa64509234e8897e
busranurbaydur <busranurbaydur@outlook.com>
2022-01-27 18:12:36 +0300

    Project Updated

--- TravelProject/Controllers/MuzelerController.cs
+++ TravelProject/Controllers/MuzelerController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelProject.Models.Siniflar;
 
 namespace TravelProject.Controllers
 {
     public class MuzelerController : Controller
     {
         // GET: Muzeler
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var muze = c.Muzes.ToList();
+            return View(muze);
         }
     }
 }
--- TravelProject/Controllers/RestoranlarController.cs
+++ TravelProject/Controllers/RestoranlarController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelProject.Models.Siniflar;
 
 namespace TravelProject.Controllers
 {
     public class RestoranlarController : Controller
     {
         // GET: Restoranlar
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var restoran = c.Restorans.ToList();
+            return View(restoran);
         }
     }
 }
